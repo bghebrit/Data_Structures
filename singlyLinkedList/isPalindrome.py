d71d9860c637bf5b6545d653a79165643aeca6be
bghebrit <sghebrit@gmail.com>
2020-07-08 23:47:14 -0700

    data structures work out

---
+++ singlyLinkedList/isPalindrome.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SLL:
+    def __init__(self):
+        self.head = None
+
+
+    def isPalindrome(self):
+        string = ''
+        currentNode = self.head
+        while currentNode.next is not None:
+            string += currentNode.data
+            currentNode = currentNode.next
+        return palindrome(string)
+
+    def palindrome(self, string):
+        i = 0
+        j = len(string)-1
+        while i < j:
+            if string[i] != string[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+
+list = SLL()
+isPal = list.palindrome('racecar')
+isPal1 = list.palindrome('hello')
+print(isPal)
+print(isPal1)
